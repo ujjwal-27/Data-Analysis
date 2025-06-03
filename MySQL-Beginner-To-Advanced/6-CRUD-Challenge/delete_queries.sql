7d5ba971eb7da00302194054200d32a984116811
Ujjwal <ujjwalshresth.96@gmail.com>
2025-06-03 09:34:09 +0545

    implement delete queries

---
+++ MySQL-Beginner-To-Advanced/6-CRUD-Challenge/delete_queries.sql
@@ -0,0 +1,8 @@
+select * from shirts where last_worn = 200;
+delete from shirts where last_worn = 200;
+
+select * from shirts where article = 'tank top';
+delete from shirts where article = 'tank top';
+
+select * from shirts;
+delete from shirts;
