42ba80a977f74b7cacd8291a3340ee3f91b89d4d
Ujjwal <ujjwalshresth.96@gmail.com>
2025-06-03 09:25:10 +0545

    implement update queries

---
+++ MySQL-Beginner-To-Advanced/6-CRUD-Challenge/update_queries.sql
@@ -0,0 +1,5 @@
+update shirts set shirt_size = 'L' where article = 'polo shirt';
+
+update shirts set last_worn = 0 where last_worn = 15; 
+
+update shirts set shirt_size = 'XS', color = 'off white' where color = 'white';
