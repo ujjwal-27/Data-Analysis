2b818b4ab5a26bb2a45bfb8596e4647accb0599b
Ujjwal <ujjwalshresth.96@gmail.com>
2025-06-12 09:30:14 +0545

    add comments

--- MySQL-Beginner-To-Advanced/7-String-Functions/substring.sql
+++ MySQL-Beginner-To-Advanced/7-String-Functions/substring.sql
@@ -2,4 +2,5 @@ select substring('Hello World', 7, 2) as new_string;
 
 select substring('Hello World', -7) as new_string;
 
+-- Using substring and combining concat and substring
 select substring(author_fname, 1, 1) as initial, author_fname, concat(substring(author_fname, 1, 1), '. ', author_lname) as name from books;
