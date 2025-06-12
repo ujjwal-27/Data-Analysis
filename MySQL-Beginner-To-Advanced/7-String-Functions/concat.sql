be2204769dc110c476bdbe8787f262ba80ca8cd1
Ujjwal <ujjwalshresth.96@gmail.com>
2025-06-12 08:26:54 +0545

    implement concat with separator

--- MySQL-Beginner-To-Advanced/7-String-Functions/concat.sql
+++ MySQL-Beginner-To-Advanced/7-String-Functions/concat.sql
@@ -4,3 +4,7 @@ SELECT CONCAT('read', ' ', 'books');
 
 -- Concat table columns with space in between
 select CONCAT(author_fname, ' ', author_lname) as full_name from books;
+
+-- Concat with separator
+-- This can basically used for creating slug from table comlumns.
+select concat_ws('-', author_fname, author_lname, released_year) as slug from books;
