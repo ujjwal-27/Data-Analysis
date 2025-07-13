24c813d219ea7d2155ea251e6138fc0dc83ca1d3
Ujjwal <ujjwalshresth.96@gmail.com>
2025-07-13 06:49:36 +0545

    select order by

--- MySQL-Beginner-To-Advanced/8-Refining-Selections/select_order_by.sql
+++ MySQL-Beginner-To-Advanced/8-Refining-Selections/select_order_by.sql
@@ -1,3 +1,8 @@
 select * from books order by pages;
 
-select * from books order by released_year desc;
+-- here, the author_lname is ordered in asc by default.
+select * from books order by author_lname, released_year desc;
+
+select * from books order by author_lname desc, released_year desc;
+
+select concat(author_fname, ' ', author_lname) as author from books order by author desc;
