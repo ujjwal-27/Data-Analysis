95cb7e4a3eb5f29c51878badbaa15c8d6ac8b25e
Ujjwal <ujjwalshresth.96@gmail.com>
2025-07-15 14:07:04 +0545

    select min max

---
+++ MySQL-Beginner-To-Advanced/9-Aggregate-Functions/select_min_max.sql
@@ -0,0 +1,9 @@
+select min(released_year), max(released_year) from books;
+
+select min(pages), max(pages) from books;
+
+select min(stock_quantity), max(stock_quantity) from books;
+
+select min(author_fname), max(author_fname) from books;
+
+select min(author_lname), max(author_lname) from books;
