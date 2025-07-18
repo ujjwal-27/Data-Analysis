b19c5940d60ad9fd108929963a197ace88267d65
Ujjwal <ujjwalshresth.96@gmail.com>
2025-07-18 20:30:54 +0545

    time diff function

--- MySQL-Beginner-To-Advanced/10 - Revisiting-Data-Types/date-math.sql
+++ MySQL-Beginner-To-Advanced/10 - Revisiting-Data-Types/date-math.sql
@@ -5,3 +5,5 @@ select date_add(now(), interval 7 month);
 select date_add(now(), interval 2 year);
 
 select birthdate, date_add(birthdate, interval 7 year) from people;
+
+select timediff(curtime(), '07:00:00');
