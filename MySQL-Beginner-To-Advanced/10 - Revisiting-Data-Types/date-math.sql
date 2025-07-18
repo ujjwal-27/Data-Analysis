577991ba557317e08ad63fd6d7b898e2b32c110d
Ujjwal <ujjwalshresth.96@gmail.com>
2025-07-18 20:14:08 +0545

    date add function

---
+++ MySQL-Beginner-To-Advanced/10 - Revisiting-Data-Types/date-math.sql
@@ -0,0 +1,7 @@
+select birthdate, datediff(curdate(), birthdate) from people;
+
+select date_add(now(), interval 5 day);
+select date_add(now(), interval 7 month);
+select date_add(now(), interval 2 year);
+
+select birthdate, date_add(birthdate, interval 7 year) from people;
