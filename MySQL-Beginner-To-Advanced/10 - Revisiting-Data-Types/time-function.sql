817906d4d047cb1af320d0c584117761bce7c4f0
Ujjwal Shrestha <ujjwalshresth.96@gmail.com>
2025-07-25 17:39:18 +0545

    minor change

--- MySQL-Beginner-To-Advanced/10 - Revisiting-Data-Types/time-function.sql
+++ MySQL-Beginner-To-Advanced/10 - Revisiting-Data-Types/time-function.sql
@@ -1,3 +1,3 @@
-select name, birthdt, hour(birthdt), minute(birthdt), second(birthdt) from people;
+Select name, birthdt, hour(birthdt), minute(birthdt), second(birthdt) from people;
 
 select name, birthdt, date(birthdt), time(birthdt) from people;
