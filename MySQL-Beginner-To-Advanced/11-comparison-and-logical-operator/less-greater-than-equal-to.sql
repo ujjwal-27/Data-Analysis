a2bf43f4ec0336f3dd58f6973019456aed48d41f
Ujjwal Shrestha <ujjwalshresth.96@gmail.com>
2025-07-28 15:40:04 +0545

    less and greater than equals to

---
+++ MySQL-Beginner-To-Advanced/11-comparison-and-logical-operator/less-greater-than-equal-to.sql
@@ -0,0 +1,5 @@
+select title, released_year from books where released_year <= 2000;
+
+select title, released_year from books where released_year >= 2010;
+
+select book_id, title, pages from books where pages <= 200;
